24f5a97ab700892a092504b8fde6bebec930be81
wittgena <wittgena@gmail.com>
2026-08-20 07:29:48 +0900

    +align.fiber: ator.runtime
    {"nexus_id":1614813188,"parent_commit_id":"0000000","parent_nexus_id":0}

--- kernel/bind/around.py
+++ kernel/bind/around.py
@@ -15,7 +15,7 @@ log = logging.getLogger("around")
 CURRENT_SCRIPT = Path(__file__).absolute()
 CURRENT_DIR = CURRENT_SCRIPT.parent
 PTH_FILENAME = "xphi.pth"
-CORES = ["fiber", "theoria", "xphi"]
+CORES = ["nexus", "fiber", "theoria", "xphi"]
 
 ## Default minimum skeleton to prevent resolver crashes if bound.json doesn't exist
 DEFAULT_BOUND_SKELETON = {
@@ -34,8 +34,8 @@ DEFAULT_BOUND_SKELETON = {
         "phase": "xphi/phase"
     },
     "paths": {
+        "theoria": ":anchor:/theoria",
         "fiber": "fiber",
-        "theoria": "theoria",
         "io": ":io:",
         "ext": ":anchor:/ext",
         "chat": ":io:/chat",
